532d0e116d058e76b5e38f428e3bd4b10e77d1a4
Vasco Dias <m+git@vascorsd.com>
2022-10-07 03:16:19 +0100

    free of the heap string

--- lib/cross.native.scala
+++ lib/cross.native.scala
@@ -1,14 +1,16 @@
 //> using target.platform "scala-native"
 
 import scala.scalanative.unsafe._
-
+import scala.scalanative.libc._
 
 object Cross:
   def stuff: String =
     "native cross compile"
 
   def currentDirectory(): String =
-    fromCString(unistd.get_current_dir_name())
+    NativeHelpers.moveIntoScalaMemory {
+      unistd.get_current_dir_name()
+    }
 
 
 @extern
@@ -17,3 +19,13 @@ object unistd:
   // if env variable PWD is set, and the value is correct, then that value is returned.
   // the caller should free the returned buffer.
   def get_current_dir_name(): CString = extern
+
+object NativeHelpers:
+  // Convert an heap allocated CString (ptr and terminated by NUL) from the C-Side 
+  // into a regular String in the Scala-Side. After the conversoin deallocates the
+  // now unneeded CString by free the memore where it was at.
+  def moveIntoScalaMemory(cs: CString): String =
+    val converted = fromCString(cs)
+    stdlib.free(cs)
+  
+    converted
